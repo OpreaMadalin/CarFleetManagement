409ba1eca838551f8b2df22303fc464f95fd22af
OpreaMadalin <madalinoprea1992@yahoo.com>
2021-08-05 17:20:21 +0300

    first commit

---
+++ src/main/java/com/company/CarFleetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarFleetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarFleetManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/CarFleetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarFleetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
